ad7b3b77651fec79e31d0c1442513e59cd7be63e
Robert Socha <socha@socha.it>
2024-06-29 22:14:59 +0200

    ws-echo-server

--- ws-server.cpp
+++ ws-server.cpp
@@ -1,61 +1,44 @@
 // g++ -march=native -O3 -Wpedantic -Wall -Wextra -Wsign-conversion -Wconversion -std=c++20 -Isrc -IuSockets/src -flto=auto -DUWS_NO_ZLIB ws-server.cpp  -s -static uSockets/*.o -o ws-server
-#include "App.h"
-#include "ws-server.h"
 #include <string>
+#include "App.h"
+#include "LocalCluster.h"
+#include "ws-server.h"
 
 int main() {
     struct PerSocketData {
-        /* Define your user data */
-        int something;
+        int version;
     };
-
-    uWS::App().get("/", [](auto *res, auto *req) {
-	  std::cout << "URL: " << req->getUrl() << "\n";
-          res->writeStatus(uWS::HTTP_200_OK);
-	  res->writeHeader("Content-Type", "text/html");
-	  res->end("<html><h1><a href=\"/client\">CLIENT</a></h1></html>");
-	}).get("/client", [](auto *res, auto /* *req */) {
-	  std::cout << "RES CONNECTION: " << res->getRemoteAddressAsText() << "\n";
-          res->writeStatus(uWS::HTTP_200_OK);
-	  res->writeHeader("Content-Type", "text/html");
-	  res->end(client);
-	}).ws<PerSocketData>("/echo", {
+    uWS::LocalCluster({},
+    [](uWS::SSLApp &app) {
+      app.get("/", [](auto *res, auto *) {
+        res->writeStatus(uWS::HTTP_200_OK);
+        res->writeHeader("Content-Type", "text/html");
+        res->end("<html><h1><a href=\"/client\">CLIENT</a></h1></html>");
+      }).get("/client", [](auto *res, auto *) {
+        res->writeStatus(uWS::HTTP_200_OK)->writeHeader("Content-Type", "text/html")->end(client);
+      }).ws<PerSocketData>("/reverse", {
         .compression = uWS::SHARED_COMPRESSOR,
         .maxPayloadLength = 16 * 1024,
         .idleTimeout = 10,
         .maxBackpressure = 1 * 1024 * 1024,
-        /* Handlers */
         .upgrade = [](auto *res, auto *req, auto *context) {
-            res->template upgrade<PerSocketData>({
-                .something = 100
-            }, req->getHeader("sec-websocket-key"),
-                req->getHeader("sec-websocket-protocol"),
-                req->getHeader("sec-websocket-extensions"),
-                context);
-        },
-        .open = [](auto *ws) {
-	   std::cout << "WS CONNECTION: " << ws->getRemoteAddressAsText() << "\n";
+          res->template upgrade<PerSocketData>({
+            .version = 1
+          }, req->getHeader("sec-websocket-key"),
+             req->getHeader("sec-websocket-protocol"),
+             req->getHeader("sec-websocket-extensions"),
+             context);
         },
         .message = [](auto *ws, std::string_view message, uWS::OpCode opCode) {
-	    std::string reversed(message.rbegin(), message.rend());
-            ws->send(std::string(message) + " = " + reversed, opCode);
-        },
-        .drain = [](auto */*ws*/) {
-            /* Check ws->getBufferedAmount() here */
-        },
-        .ping = [](auto */*ws*/, std::string_view) {
-            /* You don't need to handle this one, we automatically respond to pings as per standard */
-        },
-        .pong = [](auto */*ws*/, std::string_view) {
-            /* You don't need to handle this one either */
-        },
-        .close = [](auto */*ws*/, int /*code*/, std::string_view /*message*/) {
-            /* You may access ws->getUserData() here, but sending or
-             * doing any kind of I/O with the socket is not valid. */
+          std::string reversed(message.rbegin(), message.rend());
+          ws->send(std::string(message) + " >> " + reversed, opCode);
         }
-    }).listen(8000, [](auto *listen_socket) {
+      }).listen(8000, [](auto *listen_socket) {
         if (listen_socket) {
-            std::cout << "Listening on port " << 8000 << std::endl;
-        }
-    }).run();
+          std::cout << "Thread " << std::this_thread::get_id() << " listening on port " << us_socket_local_port(true, (struct us_socket_t *) listen_socket) << std::endl;
+         } else {
+           std::cout << "Thread " << std::this_thread::get_id() << " failed to listen on port 3000" << std::endl;
+         }
+      });
+    });
 }
--- ws-server.h
+++ ws-server.h
@@ -1,76 +1,91 @@
-std::string client = R"(
-<!DOCTYPE html>
+std::string client = R"(<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
+<title>Websocket Client ...</title>
 <script>
-window.addEventListener("load", function(evt) {
+  window.addEventListener("load", function(evt) {
     var output = document.getElementById("output");
     var input = document.getElementById("input");
     var ws;
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.textContent = message;
-        output.appendChild(d);
-        output.scroll(0, output.scrollHeight);
+    var print = function(...args) {
+      var d = document.createElement("div");
+      for (let i = 0; i < args.length; i += 2) {
+        var color = args[i];
+        var text = args[i + 1];
+        var span = document.createElement("span");
+          span.textContent = text;
+          span.style.color = color;
+          d.appendChild(span);
+      }
+      output.appendChild(d);
+      output.scroll(0, output.scrollHeight);
     };
     document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-	const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-	const host = window.location.host;
-	const wsurl = `${protocol}//${host}/echo`;
-        ws = new WebSocket(wsurl);
-        ws.onopen = function(evt) {
-            print("OPEN: " + wsurl);
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
+      if (ws) return false;
+      const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    	const host = window.location.host;
+    	const wsurl = `${protocol}//${host}/reverse`;
+      ws = new WebSocket(wsurl);
+      ws.onopen = function(evt) {
+        print("BLACK","OPEN: ","RED",wsurl);
+      }
+      ws.onclose = function(evt) {
+        print("RED","CLOSE");
+        ws = null;
+      }
+      ws.onmessage = function(evt) {
+        print("BLACK","R: ","GREEN", evt.data);
+      }
+      ws.onerror = function(evt) {
+        print("RED","ERROR: ","BLACK",evt.data);
+      }
+      return false;
     };
     document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
+      if (!ws) {
+        print("RED","ERROR: please ","GREEN","OPEN","RED"," connection first ...");
         return false;
+      }
+      print("BLACK","S: ", "BLUE", input.value);
+      ws.send(input.value);
+      return false;
     };
     document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
+      if (!ws) return false;
+      ws.close();
+      return false;
     };
-
-});
+    document.getElementById("clear").onclick = function(evt) {
+      while(output.firstChild) {
+        output.removeChild(output.firstChild);
+      }
+      console.log('clear ...');
+      return false;
+    };
+  });
 </script>
 </head>
 <body>
 <table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server, 
-"Send" to send a message to the server and "Close" to close the connection. 
-You can change the message and send multiple times.
-<p>
-<form>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
-</td></tr></table>
+  <tr>
+    <td valign="top" width="50%">
+      <p>Click "Open" to create a connection to the server, "Send" to send a message to the server and "Close" to close the connection.
+      You can change the message and send multiple times.<p>
+      <form>
+        <button id="open">Open</button>
+        <button id="close">Close</button>
+        <button id="clear">Clear</button>
+        <p><input id="input" type="text" value="Hello world!">
+        <button id="send">Send</button>
+      </form>
+    </td>
+  </tr>
+  <tr>
+    <td valign="top" width="100%">
+      <div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
+    </td>
+  </tr>
+</table>
 </body>
 </html>)";
